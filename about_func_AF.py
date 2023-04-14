ae816b4c47f84fd6269cb638db6f3dfc8ee0f3be
perfext <62498788+programm-er-games@users.noreply.github.com>
2023-04-14 23:49:16 +0300

    Перезалив

--- about_func_AF.py
+++
@@ -1,6 +0,0 @@
-def about_func(message, markup_remove):
-    text = "Данный бот создан замечательным, но скромным человеком Поповым Алексеем Эдуардовичем " \
-           "на собственном энтузиазме. По вопросам или предложениям по поводу этого бота " \
-           "обращаться к этому боту или человеку (если вдруг бот будет не работать): " \
-           "@FeedbackAboutBots_bot или @QuizBot_Developer"
-    bot.send_message(message.chat.id, text, reply_markup=markup_remove)
